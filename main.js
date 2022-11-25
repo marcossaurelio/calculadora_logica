c2e1027b2bf169310cab9b98814b7fbd03a7da17
Marcos Aurélio <116835998+marcossaurelio@users.noreply.github.com>
2022-11-25 16:21:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,83 @@
-var p = true;
-var q = true;
-var r = true;
-var s = true;
-var t = true;
-var u = true;
-var expressao = '';
+let p = true;
+let q = true;
+let r = true;
+let s = true;
+let t = true;
+let u = true;
+let expressao = '';
+
+function cleanResult() {
+    document.getElementById('resultado').innerHTML = '';
+}
 
 function insert(char) {
-    var conteudo = document.getElementById('monitor').innerHTML;
+    let conteudo = document.getElementById('monitor').innerHTML;
     document.getElementById('monitor').innerHTML = conteudo + char;
-    if(char=='&#40;'){
-        expressao += char;
-    } else if(char=='&#41;'){
-        expressao += char;
+    if(char=='\('){
+        expressao += '(';
+        cleanResult();
+    } else if(char=='\)'){
+        expressao += ')';
+        cleanResult();
     } else if(char=='∧'){
         expressao += '&&';
+        cleanResult();
     } else if(char=='∨'){
         expressao += '||'
-    } else if(char=='∼'){
+        cleanResult();
+    } else if(char=='~'){
         expressao += '!';
+        cleanResult();
     } else if(char=='→'){
-        expressao += '===';
+        expressao += '==';
+        cleanResult();
     } else if(char=='↔'){
-        expressao += '===';
+        expressao += '==';
+        cleanResult();
     } else if(char=='P'){
         expressao += 'p';
+        calcular();
     } else if(char=='Q'){
         expressao += 'q';
+        calcular();
     } else if(char=='R'){
         expressao += 'r';
+        calcular();
     } else if(char=='S'){
         expressao += 's';
+        calcular();
     } else if(char=='T'){
         expressao += 't';
+        calcular();
     } else if(char=='U'){
         expressao += 'u';
+        calcular();
     }
 }
 
 function clean() {
     document.getElementById('monitor').innerHTML = '';
     expressao = '';
-    document.getElementById('resultado').innerHTML = '';
+    cleanResult();
 }
 
 function back() {
-    var conteudo = document.getElementById('monitor').innerHTML;
+    conteudo = document.getElementById('monitor').innerHTML;
     document.getElementById('monitor').innerHTML = conteudo.substring(0, conteudo.length -1);
-    expressao = expressao.substring(0, expressao.length -1);
+    let exp = expressao;
+    if (exp.substring(exp.length - 1) == '&' || exp.substring(exp.length - 1) == '|' || exp.substring(exp.length - 1) == '=') {
+        expressao = exp.substring(0, exp.length -2);
+    } else {
+        expressao = exp.substring(0, exp.length -1);
+    }
+    cleanResult();
 }
 
 function calcular() {
-    if (eval(expressao) === 'undefined' || eval(expressao) === 'null'){
-        document.getElementById('resultado').innerHTML = 'Operação inválida';
+    if (expressao === '') {
+        clean();
+    } else if (eval(expressao) == undefined) {
+        document.getElementById('resultado').innerHTML = 'operação inválida';
     } else {
         document.getElementById('resultado').innerHTML = eval(expressao);
     }
@@ -60,69 +85,64 @@ function calcular() {
 
 function mudaEstado(letra) {
     if (letra == 'p') {
+        p = !p
         if (document.getElementById(letra).innerHTML == 'V') {
-            var p = false
             document.getElementById(letra).innerHTML = 'F';
             document.getElementById(letra).setAttribute("class", "botaof");
         } else {
-            var p = true
             document.getElementById(letra).innerHTML = 'V';
             document.getElementById(letra).setAttribute("class", "botaot");
         }
     }
     if (letra == 'q') {
+        q = !q
         if (document.getElementById(letra).innerHTML == 'V') {
-            var q = false
             document.getElementById(letra).innerHTML = 'F';
             document.getElementById(letra).setAttribute("class", "botaof");
         } else {
-            var q = true
             document.getElementById(letra).innerHTML = 'V';
             document.getElementById(letra).setAttribute("class", "botaot");
         }
     }
     if (letra == 'r') {
+        r = !r
         if (document.getElementById(letra).innerHTML == 'V') {
-            var r = false
             document.getElementById(letra).innerHTML = 'F';
             document.getElementById(letra).setAttribute("class", "botaof");
         } else {
-            var r = true
             document.getElementById(letra).innerHTML = 'V';
             document.getElementById(letra).setAttribute("class", "botaot");
         }
     }
     if (letra == 's') {
+        s = !s;
         if (document.getElementById(letra).innerHTML == 'V') {
-            var s = false
             document.getElementById(letra).innerHTML = 'F';
             document.getElementById(letra).setAttribute("class", "botaof");
         } else {
-            var s = true
             document.getElementById(letra).innerHTML = 'V';
             document.getElementById(letra).setAttribute("class", "botaot");
         }
     }
     if (letra == 't') {
+        t = !t;
         if (document.getElementById(letra).innerHTML == 'V') {
-            var t = false
             document.getElementById(letra).innerHTML = 'F';
             document.getElementById(letra).setAttribute("class", "botaof");
         } else {
-            var t = true
             document.getElementById(letra).innerHTML = 'V';
             document.getElementById(letra).setAttribute("class", "botaot");
         }
     }
     if (letra == 'u') {
+        u = !u
         if (document.getElementById(letra).innerHTML == 'V') {
-            var u = false
             document.getElementById(letra).innerHTML = 'F';
             document.getElementById(letra).setAttribute("class", "botaof");
         } else {
-            var u = true
             document.getElementById(letra).innerHTML = 'V';
             document.getElementById(letra).setAttribute("class", "botaot");
         }
     }
+    calcular();
 }
